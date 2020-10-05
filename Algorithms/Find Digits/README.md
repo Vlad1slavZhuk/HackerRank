89e236b0fe9b287a506e018139873de03e3e0cd9
Vlad1slavZhuk <33846626+Vlad1slavZhuk@users.noreply.github.com>
2020-10-05 11:48:08 +0300

    add task

---
+++ Algorithms/Find Digits/README.md
@@ -0,0 +1,5 @@
+# Find Digits
+
+Link to **HackerRank**: [HackerRank](https://www.hackerrank.com/challenges/find-digits/problem)
+
+Link to **The Play Golang**: [play.golang.org](https://play.golang.org/p/DBVNMgu-Q0Q)
---
+++ Algorithms/Find Digits/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the findDigits function below.
+func findDigits(n int32) int32 {
+	var r, count int32 = n, 0
+
+	for r > 0 {
+		if r%10 != 0 && n%(r%10) == 0 {
+			count++
+		}
+		r /= 10
+	}
+	return count
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	tTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	t := int32(tTemp)
+
+	for tItr := 0; tItr < int(t); tItr++ {
+		nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+		n := int32(nTemp)
+
+		result := findDigits(n)
+
+		fmt.Fprintf(writer, "%d\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
