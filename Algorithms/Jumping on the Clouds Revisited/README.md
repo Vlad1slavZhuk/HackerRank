fec519bce2b5323f241996bb287395759a5175b2
Vlad1slavZhuk <33846626+Vlad1slavZhuk@users.noreply.github.com>
2020-10-06 13:00:40 +0300

    add task

---
+++ Algorithms/Jumping on the Clouds Revisited/README.md
@@ -0,0 +1,5 @@
+# Jumping on the Clouds: Revisited
+
+Link to **HackerRank**: [HackerRank](https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem)
+
+Link to **The Play Golang**: [play.golang.org](https://play.golang.org/p/CJZ_t6hLcLT)
---
+++ Algorithms/Jumping on the Clouds Revisited/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the jumpingOnClouds function below.
+func jumpingOnClouds(c []int32, k int32) int32 {
+	var (
+		e    = int32(100)
+		size = int32(len(c))
+	)
+
+	for i := int32(0); i < size; i += k {
+		if i > 0 {
+			if c[i] == 1 {
+				e -= 2
+			}
+			e--
+		}
+
+		if i+k >= size {
+			i = (i + k) % size
+			e--
+			if c[i] == 1 {
+				e -= 2
+			}
+			if i == 0 {
+				break
+			}
+		}
+	}
+
+	return e
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nk := strings.Split(readLine(reader), " ")
+
+	nTemp, err := strconv.ParseInt(nk[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(nk[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	cTemp := strings.Split(readLine(reader), " ")
+
+	var c []int32
+
+	for i := 0; i < int(n); i++ {
+		cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+		checkError(err)
+		cItem := int32(cItemTemp)
+		c = append(c, cItem)
+	}
+
+	result := jumpingOnClouds(c, k)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
